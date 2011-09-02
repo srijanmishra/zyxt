ff83dd775d165d103b28aedf758a755c058df3d8
Kushagra Sinha <sinha.kushagra@gmail.com>
2011-09-03 02:38:18 +0530

    Addded static files url

--- zyxt/settings.py
+++ zyxt/settings.py
@@ -65,7 +65,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, '../../static_repo/static/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = 'http://zyxt-static.herokuapp.com/'
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
